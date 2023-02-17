ce5aab94e384e052fbc530155ca9f835d63aa537
Nice Programming <33935272+niceprogramming@users.noreply.github.com>
2023-02-17 10:41:56 -0600

    Setup DI with parser and command

--- GrInterview.Console/Commands/ParseCommand.cs
+++ GrInterview.Console/Commands/ParseCommand.cs
@@ -6,14 +6,23 @@ using System.Threading.Tasks;
 using CliFx;
 using CliFx.Attributes;
 using CliFx.Infrastructure;
+using GrInterview.Common.Interfaces;
+using GrInterview.Common.Models;
 
 namespace GrInterview.Console.Commands
 {
     [Command]
     public class ParseCommand : ICommand
     {
+        private readonly IParser<User> _parser;
+
         [CommandParameter(0)] 
         public IReadOnlyList<FileInfo> Files { get; init; } = new List<FileInfo>();
+
+        public ParseCommand(IParser<User> parser)
+        {
+            _parser = parser;
+        }
 
         public ValueTask ExecuteAsync(IConsole console)
         {
--- GrInterview.Console/Program.cs
+++ GrInterview.Console/Program.cs
@@ -1,8 +1,14 @@
-﻿using CliFx;
+﻿using System.Xml.Serialization;
+using CliFx;
+using GrInterview.Common.Interfaces;
+using GrInterview.Common.Models;
+using GrInterview.Common.Parsers;
 using GrInterview.Console.Commands;
 using Microsoft.Extensions.DependencyInjection;
 
 var services = new ServiceCollection();
+
+services.AddSingleton<IParser<User>, UserParser>();
 
 services.AddTransient<ParseCommand>();
 var serviceProvider = services.BuildServiceProvider();
