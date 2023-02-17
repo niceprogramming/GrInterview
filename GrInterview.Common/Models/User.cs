ad933214f5c9fc20f09e7cc3d20fd4db3f146b0f
Nice Programming <33935272+niceprogramming@users.noreply.github.com>
2023-02-17 10:25:58 -0600

    Initial parsing structure

---
+++ GrInterview.Common/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrInterview.Common.Models
+{
+    public class User
+    {
+        public string LastName { get; }
+        public string FirstName { get; }
+        public string Email { get; }
+        public string FavoriteColor { get; }
+        public string DateOfBirth { get; }
+
+        public User(string lastName, string firstName, string email, string favoriteColor, string dateOfBirth)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            Email = email;
+            FavoriteColor = favoriteColor;
+            DateOfBirth = dateOfBirth;
+        }
+    }
+}
---
+++ GrInterview.Common/Parsers/PersonParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GrInterview.Common.Interfaces;
+using GrInterview.Common.Models;
+
+namespace GrInterview.Common.Parsers
+{
+    public class UserParser : IParser<User>
+    {
+        public Task<User> Parse(StreamReader reader)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
