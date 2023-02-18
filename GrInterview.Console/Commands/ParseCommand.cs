7cae0be8ba7c5c396ac07b57f022594c816ae2ee
Nice Programming <33935272+niceprogramming@users.noreply.github.com>
2023-02-18 14:23:05 -0600

    implement ParseCommand plus initial tests

--- GrInterview.Console/Commands/ParseCommand.cs
+++ GrInterview.Console/Commands/ParseCommand.cs
@@ -1,34 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using CliFx;
 using CliFx.Attributes;
+using CliFx.Exceptions;
 using CliFx.Infrastructure;
+using ConsoleTables;
 using GrInterview.Common.Interfaces;
 using GrInterview.Common.Models;
 
-namespace GrInterview.Console.Commands
+namespace GrInterview.Console.Commands;
+
+[Command]
+public class ParseCommand : ICommand
 {
-    [Command]
-    public class ParseCommand : ICommand
+    private readonly IParser<User> _parser;
+
+    [CommandParameter(0)]
+    public IReadOnlyList<FileInfo> Files { get; init; } = new List<FileInfo>();
+
+    public ParseCommand(IParser<User> parser)
     {
-        private readonly IParser<User> _parser;
+        _parser = parser;
+    }
 
-        [CommandParameter(0)] 
-        public IReadOnlyList<FileInfo> Files { get; init; } = new List<FileInfo>();
-
-        public ParseCommand(IParser<User> parser)
+    public async ValueTask ExecuteAsync(IConsole console)
+    {
+        var records = new List<User>();
+        foreach (var file in Files)
         {
-            _parser = parser;
+            if (!file.Exists)
+            {
+                throw new CommandException($"{file.Name} doesn't exist in directory: {file.DirectoryName}");
+            }
+
+            using var reader = File.OpenText(file.FullName);
+
+            var result = await _parser.Parse(reader);
+            records.AddRange(result);
         }
 
-        public ValueTask ExecuteAsync(IConsole console)
-        {
-            console.Output.WriteLine("Parsed");
+        var headers = typeof(User)
+            .GetProperties()
+            .Select(x => x.Name);
 
-            return default;
+        var table = new ConsoleTable(new ConsoleTableOptions
+        {
+            OutputTo = console.Output,
+            EnableCount = false,
+            Columns = headers
+        });
+
+        var firstOutput = records
+            .OrderBy(x => x.FavoriteColor.ToLower())
+            .ThenBy(x => x.LastName);
+
+        WriteToConsole(table, firstOutput);
+
+        var secondOutput = records
+            .OrderBy(x => x.DateOfBirth);
+
+        WriteToConsole(table, secondOutput);
+
+        var thirdOutput = records
+            .OrderByDescending(x => x.LastName.ToLower());
+
+        WriteToConsole(table, thirdOutput);
+    }
+
+    private void WriteToConsole(ConsoleTable table, IEnumerable<User> records)
+    {
+        foreach (var record in records)
+        {
+            table.AddRow(record.LastName, record.FirstName, record.Email, record.FavoriteColor,
+                record.DateOfBirth.ToString("M/d/yyyy"));
         }
+
+        table.Write();
+        table.Rows.Clear();
     }
 }
---
+++ GrInterview.Tests/ParseCommandTest.cs
@@ -0,0 +1,56 @@
+﻿using GrInterview.Common.Models;
+using GrInterview.Common.Parsers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CliFx.Exceptions;
+using CliFx.Infrastructure;
+using GrInterview.Console.Commands;
+
+namespace GrInterview.Tests
+{
+    public class ParseCommandTest
+    {
+        [Fact]
+        public async Task Command_executes_successfully_given_an_existing_file_and_outputs_sorted_data()
+        {
+            using var console = new FakeInMemoryConsole();
+            var parser = new UserParser(new[] { ",", "|", " " });
+            var command = new ParseCommand(parser)
+            {
+                Files = new List<FileInfo>
+                {
+                    new("./TestFiles/FullTest.txt")
+                }
+            };
+
+            var expected = TestData.SuccessfulTestOutput;
+            await command.ExecuteAsync(console);
+
+            var result = console.ReadOutputString();
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public async Task Command_throws_exception_given_a_nonexistent_file()
+        {
+            using var console = new FakeInMemoryConsole();
+            var parser = new UserParser(new[] { ",", "|", " " });
+            var command = new ParseCommand(parser)
+            {
+                Files = new List<FileInfo>
+                {
+                    new("thisfiledoesntexist.txt")
+                }
+            };
+           
+            await Assert.ThrowsAnyAsync<CommandException>( async () => await command.ExecuteAsync(console));
+
+        }
+    }
+
+}
+
