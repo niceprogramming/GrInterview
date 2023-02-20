85eb221908a0f92933930c54788764099851eaa9
Nice Programming <33935272+niceprogramming@users.noreply.github.com>
2023-02-20 10:23:57 -0600

    initial setup for records controller

---
+++ GrInterview.Api/Controllers/RecordsController.cs
@@ -0,0 +1,29 @@
+﻿using GrInterview.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GrInterview.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecordsController : ControllerBase
+    {
+        // GET: api/<RecordsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+
+       
+        [HttpPost]
+        public void Post([FromBody] DelimitedRecord record)
+        {
+           
+        }
+
+
+    }
+}
---
+++ GrInterview.Api/Models/DelimitedRecord.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GrInterview.Api.Models
+{
+    public class DelimitedRecord
+    {
+        [Required]
+        public string? Data { get; init; }
+    }
+}
