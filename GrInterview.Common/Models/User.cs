c656fd0bb65a87b27e476505323398e46ced71ae
Nice Programming <33935272+niceprogramming@users.noreply.github.com>
2023-02-21 10:28:58 -0600

    change user to record. since this generate equality operators for us this make testing easier

--- GrInterview.Common/Models/User.cs
+++ GrInterview.Common/Models/User.cs
@@ -6,21 +6,5 @@ using System.Threading.Tasks;
 
 namespace GrInterview.Common.Models
 {
-    public class User
-    {
-        public string LastName { get; }
-        public string FirstName { get; }
-        public string Email { get; }
-        public string FavoriteColor { get; }
-        public DateTime DateOfBirth { get; }
-
-        public User(string lastName, string firstName, string email, string favoriteColor, DateTime dateOfBirth)
-        {
-            LastName = lastName;
-            FirstName = firstName;
-            Email = email;
-            FavoriteColor = favoriteColor;
-            DateOfBirth = dateOfBirth;
-        }
-    }
+    public record User(string LastName, string FirstName, string Email, string FavoriteColor, DateTime DateOfBirth);
 }
